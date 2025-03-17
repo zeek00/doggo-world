182ced4a77a74a65ebb9975a8d1b1e1296f051e7
zeek00 <44764168+zeek00@users.noreply.github.com>
2025-03-17 22:20:59 +0000

    refinements: 1> implemented breeds/[group]

--- app/(showcase)/breeds/[group]/page.tsx
+++ app/(showcase)/breeds/[group]/page.tsx
@@ -1,3 +1,9 @@
+import styles from "@/components/Pet/Pet.module.css";
+import Card from "@/components/ui/Card/Card";
+import {fetchPets} from "@/lib/api";
+import {getPetsByGroup} from "@/lib/utils";
+import {Suspense} from "react";
+import Loading from "@/components/loader/Loading";
 
 type PageProps = {
     params: Promise<{group: string}>;
@@ -5,8 +11,26 @@ type PageProps = {
 
 export default async function PetGroupPage({params} : PageProps) {
     const {group} = await params;
+    try{
+        const pets = await fetchPets();
+        const petGroups = await getPetsByGroup(group, pets)
+        console.log(petGroups);
+        return (
+            <Suspense fallback={<Loading />}>
+                <div className={styles.container}>
+                    <h1>{group}</h1>
+                    <div className={styles.card}>
+                        {petGroups.map((pet) => (
+                            <Card key={pet.id} {...pet} />
+                        ))}
+                    </div>
+                </div>
+            </Suspense>
 
-    return (<p>{group}</p>);
+        );
+    }catch(error){
+
+    }
 
 
 }
