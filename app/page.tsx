37fd13c32e9aebc679f1a71508d5dece9a0a7377
zeek00 <44764168+zeek00@users.noreply.github.com>
2025-03-27 14:38:39 +0000

    refinements: 1> added loading screen to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import {preload} from "@/lib/api";
+import Loading from "@/components/loader/Loading";
 
 export default async function Home() {
     preload();
 
     return (
         <div className={`flex justify-center align-items-center mt-20 `}>
-
+            <Loading/>
         </div>
     );
 }
