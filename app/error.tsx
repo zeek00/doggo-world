12eba4051746a580f67f7f37222054f957674d13
zeek00 <44764168+zeek00@users.noreply.github.com>
2025-04-21 11:41:29 +0100

    refinements: 1> added search components to breeds/ routes 2> added a global error boundary ui

--- app/error.tsx
+++ app/error.tsx
@@ -4,8 +4,9 @@ import { useRouter } from 'next/navigation'
 import styles from './error.module.css'
 import { FaShieldDog } from "react-icons/fa6";
 
-export default function PageError({ error } : Readonly<{
+export default function PageError({ error, reset } : Readonly<{
     error: Error & { digest?: string }
+    reset: () => void
 }>) {
     useEffect(() => {
         console.error(error)
@@ -22,7 +23,7 @@ export default function PageError({ error } : Readonly<{
             <span><FaShieldDog/></span>
             <h2>Oops! This pup ran off...</h2>
             <p>Looks like the page you're sniffing for doesn't exist. Try heading back to the main kennel !</p>
-
+            <button className={styles.reset} onClick={() => reset()}>Try again</button>
             <button onClick={goBack}>Return to Previous</button>
         </div>
     )
